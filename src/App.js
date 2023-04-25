69c8a7287c656aaa0bc94c0704954e489c6a2eb4
Rahulbbbb <beingbankoti@gmail.com>
2023-04-26 00:41:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductCard from './components/productcard';
+import './components/productcard.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+     <ProductCard
+         productId ="1"
+         image="https://m.media-amazon.com/images/I/6156upwZ+XL._UY695_.jpg"
+         productName ="Shoes"
+         Brand ="Mico"
+         productDescription ="The new sneakers are comfortable and stylish."
+         Price ="$50"
+         
+      />
+      <ProductCard
+         productId ="2"
+         image="https://m.media-amazon.com/images/I/71On2XSPuRL._AC_UL600_FMwebp_QL65_.jpg"
+         productName ="Shirt"
+         Brand ="Lymio"
+         productDescription ="The cotton T-shirt, soft and breathable."
+         Price ="$30"
+         AddToCart ="AddToCart"
+      />
+      <ProductCard
+         productId ="3"
+         image="https://m.media-amazon.com/images/I/61NiR2F2uML._AC_UY327_FMwebp_QL65_.jpg"
+         productName ="Cycle"
+         Brand ="MTB"
+         productDescription ="Experience the thrill of the ride - cycle"
+         
+        
+         Price ="$20"
+      />
+      <ProductCard
+         productId ="4"
+         image="https://m.media-amazon.com/images/I/4163hMQXsQL._AC_UL600_FMwebp_QL65_.jpg"
+         productName ="Dumbell"
+         Brand ="Strauss"
+         productDescription ="Build strength and transform your physique - dumbbells!"
+         Price ="$5"
+         AddToCart ="AddToCart"
+      />
+      <ProductCard
+         productId ="5"
+         image="https://m.media-amazon.com/images/I/61gC+iHYruL._AC_UL600_FMwebp_QL65_.jpg"
+         productName ="Football"
+         Brand ="Adidas"
+         productDescription ="Unleash your inner champion - play football"
+         Price ="$10"
+         AddToCart="AddToCart"
+      />
+
     </div>
   );
 }
