829e1152908c8612cc262de9deade21dc818bbf4
Rahulbbbb <beingbankoti@gmail.com>
2023-05-02 23:18:30 +0530

    first commit

---
+++ src/component/NewDogImg.js
@@ -0,0 +1,35 @@
+import { useState,useEffect } from "react";
+import './NewDogImg.css'
+
+function NewDogImg(){
+
+    const [imgUrl, setImgUrl] = useState("");
+
+    useEffect(()=>{
+        
+        async function ImgDog(){
+            try{
+            const data= await fetch('https://dog.ceo/api/breeds/image/random');
+            const NewData=await data.json();
+            setImgUrl(NewData.message);
+
+        }
+        catch(error){
+            console.log(error)
+
+        }}
+        ImgDog();
+    },[])
+  
+
+
+    return(
+        
+       <div>
+       <img src={imgUrl} alt="dog"/>
+      
+       </div>
+    )
+}
+
+export default NewDogImg
